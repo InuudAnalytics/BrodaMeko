b5f802b41610243bbf4d7d33ac371abe68bdf0bf
JustMebi <okeke.iruoma@gmail.com>
2026-03-13 18:34:43 +0100

    Fixed possible app crash cause

--- ios/BrodaMekoBare/AppDelegate.swift
+++ ios/BrodaMekoBare/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "BrotherMeko",
+      withModuleName: "BrodaMeko",
       in: window,
       launchOptions: launchOptions
     )
