ab7a70d5c109430fed5483bd017d5dfe75c181c4
JustMebi <okeke.iruoma@gmail.com>
2026-03-18 17:10:14 +0100

    chore(auth): update Google web client ID for Firebase project

--- src/config/google.js
+++ src/config/google.js
@@ -3,7 +3,7 @@ export const GOOGLE_CONFIG = {
   // Create an OAuth 2.0 Client ID of type "Web application"
   // This is required even for mobile apps to verify the token on the backend
   webClientId:
-    '209768529550-lrtb1gm7l3rammg8spr83gv9vnsj27sa.apps.googleusercontent.com',
+    '556092733214-57knktituk60nfg8rb6l35ovavs2hudu.apps.googleusercontent.com',
 
   // Optional: Only needed if you want to support offline access (getting refresh tokens)
   offlineAccess: true,
